88141f00226cee5bc4d72b17d673ff8c088b6191
andre <andre.komdeur@gmail.com>
2020-04-20 11:44:41 +0200

    class DataSourceTest

--- src/test/java/be/vdab/fietsacademy/FietsacademyApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package be.vdab.fietsacademy;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-@SpringBootTest
-class FietsacademyApplicationTests {
-    @Test
-    void contextLoads() {
-    }
-}
